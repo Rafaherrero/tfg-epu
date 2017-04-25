c8dd44d1f5f671fffd3e80821a60214e71df9685
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2017-04-25 10:57:29 +0100

    Añadiendo funcionalidad botones niveles

---
+++ code/js/myjs.js
@@ -0,0 +1,13 @@
+
+
+$("#button_level1").click(function() {
+    change_level(1);
+})
+
+$("#button_level2").click(function() {
+    change_level(2);
+})
+
+$("#button_level3").click(function() {
+    change_level(3);
+})
