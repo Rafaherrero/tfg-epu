e8f75853056fee221a905f9ec53c99a367ee3e58
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2017-04-30 20:42:11 +0100

    Probando cambios de idiomas. Cambiado css para valores calculados. Blockly en local

--- code/js/myjs.js
+++ code/js/myjs.js
@@ -5,6 +5,14 @@ function change_level(num_level){
     $('#button_level'+num_level).attr('class', 'pure-button button-xlarge pure-button-active');
 }
 
+function change_language(id_language){
+
+}
+
 $("[id^=button_level]").click(function(event) {
     change_level(event.target.id.slice(-1))
+})
+
+$(".language_buttons").click(function(event) {
+    change_language(event.target.id)
 })
