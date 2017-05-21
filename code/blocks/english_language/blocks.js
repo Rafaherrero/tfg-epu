af7b4f2789b99b4844e60b90d6181dea05c524fd
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2017-05-21 23:17:21 +0100

    Añadiendo bloques. Terminando ejercicios primer nivel

--- code/blocks/english_language/blocks.js
+++ code/blocks/english_language/blocks.js
@@ -13,7 +13,7 @@ Blockly.Blocks['english_language_dish'] = {
 
 Blockly.JavaScript['english_language_dish'] = function(block) {
     var value_put_in_dish = Blockly.JavaScript.valueToCode(block, 'put_in_dish', Blockly.JavaScript.ORDER_ATOMIC);
-    var code = 'put_in_dish'+value_put_in_dish+';';
+    var code = 'put_in_dish'+value_put_in_dish+';\n';
     return code;
 };
 
@@ -72,6 +72,50 @@ Blockly.Blocks['english_language_banana_dish'] = {
 };
 
 Blockly.JavaScript['english_language_banana_dish'] = function(block) {
-  var code = 'put_in_dish("banana");';
+  var code = 'put_in_dish("banana");\n';
+  return code;
+};
+
+///////////////////////////////////////////////////////////////////////////////////
+
+Blockly.Blocks['english_language_fruits_dish'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Put the ")
+        .appendField(new Blockly.FieldDropdown([["apple","apple-1"], ["avocado","avocado"], ["banana","banana"], ["cherry","cherries"], ["coconut","coconut"], ["grapes","grapes"], ["lemon","lemon-1"], ["orange","orange"], ["peach","peach"], ["pear","pear"], ["pineapple","pineapple"], ["raspberry","raspberry"], ["strawberry","strawberry"], ["watermelon","watermelon"]]), "fruit_dropdown")
+        .appendField("fruit in dish");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.JavaScript['english_language_fruits_dish'] = function(block) {
+  var dropdown_fruit_dropdown = block.getFieldValue('fruit_dropdown');
+  var code = 'put_in_dish("'+dropdown_fruit_dropdown+'");\n';
+  return code;
+};
+
+///////////////////////////////////////////////////////////////////////////////////
+
+Blockly.Blocks['english_language_vegetables_dish'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Put the")
+        .appendField(new Blockly.FieldDropdown([["asparagus","asparagus"], ["broccoli","broccoli"], ["cabbage","cabbage"], ["carrot","carrot"], ["cauliflower","cauliflower"], ["cucumber","cucumber"], ["garlic","garlic"], ["lettuce","salad-1"], ["onion","onion-1"], ["peas","peas"], ["pepper","pepper"], ["potato","potatoes-2"], ["radish","radish"], ["tomato","tomato"]]), "vegetables_dropdown")
+        .appendField("vegetable in dish");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.JavaScript['english_language_vegetables_dish'] = function(block) {
+  var dropdown_vegetables_dropdown = block.getFieldValue('vegetables_dropdown');
+  var code = 'put_in_dish("'+dropdown_vegetables_dropdown+'");\n';
   return code;
 };
