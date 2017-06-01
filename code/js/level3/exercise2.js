8ab4288dac86edd08e2314bf931ac198f5a189ce
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2017-06-01 13:46:18 +0100

    Terminado ejercicio 2, nivel 3

---
+++ code/js/level3/exercise2.js
@@ -0,0 +1,25 @@
+if (($('.food_in_dish').length)!=9)
+    show_solution('text_2')
+
+else{
+    var different = false;
+
+    for (var i=0; i<9&&!different; i++){
+        if ( $('#food_'+i).attr('alt')!='apple-1' && $('#food_'+i).attr('alt')!='salad-1' && $('#food_'+i).attr('alt')!='orange' ){
+            different=true;
+            show_solution('text_4')
+        }
+    }
+
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    var includes = code.includes("for(var")
+    console.log(code)
+    console.log(includes)
+    console.log(different)
+
+    if(!different&&includes)
+        show_solution('text_1')
+    else if (!different&&!includes)
+        show_solution('text_3')
+
+}
